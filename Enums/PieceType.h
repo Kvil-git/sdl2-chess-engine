286fe958b295ec57049f660bb63787cc6f076949
Kvil <kiridohard@gmail.com>
2025-08-22 17:10:34 +0200

    add ostream support to output enum typed variables

--- Enums/PieceType.h
+++ Enums/PieceType.h
@@ -1,6 +1,7 @@
 #ifndef ENUMS_PIECETYPE
 #define ENUMS_PIECETYPE
 
+#include <iostream>
 
 enum class ChessPiece {
     WhitePawn = 0,
@@ -16,8 +17,7 @@ enum class ChessPiece {
     BlackRook = 9,
     BlackQueen = 10,
     BlackKing = 11,
-    NONE = 12,
-    
+    NONE = 12,  
 };
 
 enum class PieceType {
@@ -28,6 +28,40 @@ enum class PieceType {
     Queen = 4,
     King = 5
 };
+
+
+
+std::ostream& operator<<(std::ostream& out, const ChessPiece piece) {
+    switch(piece) {
+        case ChessPiece::WhitePawn:   out << "WhitePawn"; break;
+        case ChessPiece::WhiteKnight: out << "WhiteKnight"; break;
+        case ChessPiece::WhiteBishop: out << "WhiteBishop"; break;
+        case ChessPiece::WhiteRook:   out << "WhiteRook"; break;
+        case ChessPiece::WhiteQueen:  out << "WhiteQueen"; break;
+        case ChessPiece::WhiteKing:   out << "WhiteKing"; break;
+        case ChessPiece::BlackPawn:   out << "BlackPawn"; break;
+        case ChessPiece::BlackKnight: out << "BlackKnight"; break;
+        case ChessPiece::BlackBishop: out << "BlackBishop"; break;
+        case ChessPiece::BlackRook:   out << "BlackRook"; break;
+        case ChessPiece::BlackQueen:  out << "BlackQueen"; break;
+        case ChessPiece::BlackKing:   out << "BlackKing"; break;
+        case ChessPiece::NONE:        out << "NONE"; break;
+    }
+    return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const PieceType type) {
+    switch(type) {
+        case PieceType::Pawn:   out << "Pawn"; break;
+        case PieceType::Knight: out << "Knight"; break;
+        case PieceType::Bishop: out << "Bishop"; break;
+        case PieceType::Rook:   out << "Rook"; break;
+        case PieceType::Queen:  out << "Queen"; break;
+        case PieceType::King:   out << "King"; break;
+    }
+    return out;
+}
+
 
 
 #endif
